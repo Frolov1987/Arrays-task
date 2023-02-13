efc3c28d83a44656f8140794254cc0c92ad9c80e
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-02-13 21:41:14 +0200

    Initial commit

---
+++ src/Order.java
@@ -0,0 +1,15 @@
+public class Order {
+
+    static String[] products;
+    static int ctn;
+
+    public static void main(String[] args) {
+
+        products = new String[]{"milk", "water", "buckwheat", "oil", "bear"};
+
+        for (String product : products) {
+            ctn++;
+            System.out.println(ctn + ") " + product);
+        }
+    }
+}
