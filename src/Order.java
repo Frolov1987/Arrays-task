3fb407b1be0e83982424a18f4857d1492a471bc4
Frolov1987 <116515964+Frolov1987@users.noreply.github.com>
2023-02-13 21:45:30 +0200

    add juice

--- src/Order.java
+++ src/Order.java
@@ -5,7 +5,7 @@ public class Order {
 
     public static void main(String[] args) {
 
-        products = new String[]{"milk", "water", "buckwheat", "oil", "bear"};
+        products = new String[]{"milk", "water", "buckwheat", "oil", "bear", "juce"};
 
         for (String product : products) {
             ctn++;
